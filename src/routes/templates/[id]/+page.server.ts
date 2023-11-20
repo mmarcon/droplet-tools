af0f918e3b3ff869400825d16af95b48e035a476
Massimiliano Marcon <massimiliano.marcon@mongodb.com>
2023-11-20 15:12:03 +0100

    Check lenght of array

--- src/routes/templates/[id]/+page.server.ts
+++ src/routes/templates/[id]/+page.server.ts
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
   const { rows } = await sql`SELECT id, title, description, bootstrap FROM droplet_templates WHERE id = ${params.id}`;
-  if (rows) {
+  if (rows && rows.length > 0) {
     return { template: rows[0] };
   }
   throw error(404, 'Not found');
